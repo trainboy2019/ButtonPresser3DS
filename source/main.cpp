005c4f3292ac7429e8286b4471c60de5b6002484
trainboy2019 <ikerolader@gmail.com>
2017-06-18 18:59:59 -0400

    AudioTest

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,9 @@
 #include <3ds.h>
 #include <sf2d.h>
 #include <sfil.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
 
 #include "button_png.h"
 #include "pressedButton_png.h"
@@ -21,6 +24,112 @@ int main() {
     
     int score = 0;
     bool pressed = false;
+    
+    
+    
+    
+    constexpr int channel = 1;
+    
+    u32 sampleRate;
+    u32 dataSize;
+    u16 channels;
+    u16 bitsPerSample;
+    
+    // Initialize ndsp
+    ndspInit();
+    
+    ndspSetOutputMode(NDSP_OUTPUT_STEREO);
+    ndspSetOutputCount(1); // Num of buffers
+    
+    // Reading wav file
+    FILE* fp = fopen("./click.wav", "rb");
+    
+    if(!fp)
+    {
+        printf("Could not open the example.wav file.\n");
+        return 1;
+    }
+    
+    char signature[4];
+    
+    fread(signature, 1, 4, fp);
+    
+    if( signature[0] != 'R' &&
+       signature[1] != 'I' &&
+       signature[2] != 'F' &&
+       signature[3] != 'F')
+    {
+        printf("Wrong file format.\n");
+        fclose(fp);
+        return 1;
+    }
+    
+    fseek(fp, 40, SEEK_SET);
+    fread(&dataSize, 4, 1, fp);
+    fseek(fp, 22, SEEK_SET);
+    fread(&channels, 2, 1, fp);
+    fseek(fp, 24, SEEK_SET);
+    fread(&sampleRate, 4, 1, fp);
+    fseek(fp, 34, SEEK_SET);
+    fread(&bitsPerSample, 2, 1, fp);
+    
+    if(dataSize == 0 || (channels != 1 && channels != 2) ||
+       (bitsPerSample != 8 && bitsPerSample != 16))
+    {
+        printf("Corrupted wav file.\n");
+        fclose(fp);
+        return 1;
+    }
+    
+    // Allocating and reading samples
+    u8* data = static_cast<u8*>(linearAlloc(dataSize));
+    
+    if(!data)
+    {
+        fclose(fp);
+        return 1;
+    }
+    
+    fseek(fp, 44, SEEK_SET);
+    fread(data, 1, dataSize, fp);
+    fclose(fp);
+    
+    // Find the right format
+    u16 ndspFormat;
+    
+    if(bitsPerSample == 8)
+    {
+        ndspFormat = (channels == 1) ?
+        NDSP_FORMAT_MONO_PCM8 :
+        NDSP_FORMAT_STEREO_PCM8;
+    }
+    else
+    {
+        ndspFormat = (channels == 1) ?
+        NDSP_FORMAT_MONO_PCM16 :
+        NDSP_FORMAT_STEREO_PCM16;
+    }
+    
+    ndspChnReset(channel);
+    ndspChnSetInterp(channel, NDSP_INTERP_NONE);
+    ndspChnSetRate(channel, float(sampleRate));
+    ndspChnSetFormat(channel, ndspFormat);
+    
+    // Create and play a wav buffer
+    ndspWaveBuf waveBuf;
+    std::memset(&waveBuf, 0, sizeof(ndspWaveBuf));
+    
+    //waveBuf.data_vaddr = reinterpret_cast<u32>(data);
+    waveBuf.nsamples = dataSize / (bitsPerSample >> 3);
+    waveBuf.looping = true; // Loop enabled
+    waveBuf.status = NDSP_WBUF_FREE;
+    
+    DSP_FlushDataCache(data, dataSize);
+    
+    
+    
+    
+    
 
 	// Main loop
 	while (aptMainLoop()) {
@@ -32,12 +141,14 @@ int main() {
 		if (kDown & KEY_TOUCH) {
 			pressed=true;
             score=score+1;
+            ndspChnWaveBufAdd(channel, &waveBuf);
 		}
         if (kHeld & KEY_TOUCH){
             pressed=true;
         }
         if (kUp & KEY_TOUCH){
             pressed=false;
+            ndspChnWaveBufClear(channel);
         }
 
 		if (kDown & KEY_START) {
@@ -70,6 +181,11 @@ int main() {
     sf2d_free_texture(unpressedButton);
     sf2d_free_texture(pressedButton);
 	sf2d_fini();
+    ndspChnWaveBufClear(channel);
+    
+    linearFree(data);
+    
+    ndspExit();
 
 	return 0;
 }
